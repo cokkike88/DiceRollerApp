1010fd3e188c2f15d16ca8b83c8e6f05cce6cee8
Oscar M <omunoz@healthcare.com>
2022-01-19 18:23:34 -0600

    Addes some function to show images insted the dice number

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,27 +1,29 @@
 package com.example.dicerollerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allos the user to roll a dice and view the result on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
